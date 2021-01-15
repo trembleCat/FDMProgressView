92b150718e7d747f4a5c79711c2d0f1f9cdc7113
发抖喵 <fa_dou_miao@163.com>
2021-01-15 09:08:19 +0800

    完成进度条工具

---
+++ FDMProgressViewDemo/FDMProgressView/FDMProgressView.swift
@@ -0,0 +1,288 @@
+//
+//  FDMProgressView.swift
+//  SXReader_2020
+//
+//  Created by 发抖喵 on 2021/1/7.
+//
+//  控制按钮的宽高，根据当前视图的高度而定。 目前只支持横向
+
+import UIKit
+
+//MARK: - FDMProgressViewDelegate
+@objc protocol FDMProgressViewDelegate: NSObjectProtocol {
+    
+    @objc func progressView(_ progressView: FDMProgressView, didDraggingProgressView currentValue: CGFloat)
+    
+    @objc optional func progressView(_ progressView: FDMProgressView, didTouchBeginDragView currentValue: CGFloat)
+    
+    @objc optional func progressView(_ progressView: FDMProgressView, didTouchEndDragView currentValue: CGFloat)
+}
+
+
+//MARK: - 自定义进度条
+class FDMProgressView: UIView {
+    
+    weak var delegate: FDMProgressViewDelegate?
+    
+    /// 进度条高度
+    var progressHeight: CGFloat = 8 { didSet { resetProgressView() } }
+    
+    /// 拖拽速度
+    var dragSpeed: CGFloat = 1.0
+    
+    /// 当前播放进度
+    var currentValue: CGFloat = 0
+    
+    /// 当前加载进度
+    var loadValue: CGFloat = 0
+    
+    /// 拖动控制子视图Size - 默认小圆点Size
+    var controlSize = CGSize(width: 16, height: 16) { didSet { setSubControlViewCenter() } }
+    
+    /// 总进度颜色
+    var totalProgressColor: UIColor? { didSet { totalProgressView.backgroundColor = totalProgressColor } }
+    
+    /// 加载进度颜色
+    var loadProgressColor: UIColor? { didSet { loadProgressView.backgroundColor = loadProgressColor } }
+    
+    /// 当前进度颜色
+    var currentProgressColor: UIColor? { didSet { currentProgressView.backgroundColor = currentProgressColor } }
+    
+    /// 总进度
+    private let totalProgressView = UIView()
+    
+    /// 加载进度
+    private let loadProgressView = UIView()
+    
+    /// 当前进度
+    private let currentProgressView = UIView()
+    
+    /// 拖动控制视图
+    private let dragControlView = UIView()
+    
+    /// 拖动控制起点
+    private var startValue: CGFloat?
+    
+    /// 拖动控制X值
+    private var dragControlX: CGFloat?
+    
+    /// 拖动控制子视图 - 默认为小圆点
+    private var controlView = UIView()
+    
+    /// 是否修改进度视图
+    private var shouldChangeProgress = true
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        createUI()
+        createAction()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+//MARK: - PublicAction ------------------------------------------------------------------
+extension FDMProgressView {
+    /**
+     设置当前播放进度值
+     
+     - parameter value: 播放进度
+     */
+    func setCurrentProgressValue(_ value: CGFloat) {
+        var progressValue = value
+        
+        if progressValue <= 0 { progressValue = 0 }
+        if progressValue >= 1 { progressValue = 1 }
+        
+        currentValue = progressValue
+        
+        guard shouldChangeProgress else { return }
+        let progressWidth = currentValue * totalProgressView.bounds.width
+        currentProgressView.frame = CGRect(origin: .zero, size: .init(width: progressWidth, height: progressHeight))
+        dragControlView.center.x = progressWidth + dragControlView.bounds.height * 0.5
+    }
+    
+    /**
+     设置加载进度值
+     
+     - parameter value: 加载进度
+     */
+    func setLoadProgressValue(_ value: CGFloat) {
+        var progressValue = value
+        
+        if progressValue <= 0 { progressValue = 0 }
+        if progressValue >= 1 { progressValue = 1 }
+        
+        loadValue = progressValue
+        
+        let progressWidth = loadValue * totalProgressView.bounds.width
+        loadProgressView.frame = CGRect(origin: .zero, size: .init(width: progressWidth, height: progressHeight))
+    }
+    
+    /**
+     设置控制按钮子视图
+     
+     - 控制按钮的点击范围不会发生变化
+     - parameter view: 子视图
+     - parameter size: 子视图大小
+     */
+    func setControlSubView(_ view: UIView, size: CGSize) {
+        controlView.removeFromSuperview()
+        
+        controlView = view
+        controlSize = size
+        self.dragControlView.addSubview(controlView)
+    }
+    
+    /**
+     重新布局子视图
+     */
+    func resetProgressView() {
+        let currentBounds = self.bounds
+        
+        /* 总进度 */
+        totalProgressView.layer.cornerRadius = progressHeight * 0.5
+        totalProgressView.frame = CGRect(x: currentBounds.height * 0.5,
+                                         y: currentBounds.height * 0.5 - progressHeight * 0.5,
+                                         width: currentBounds.width - currentBounds.height,
+                                         height: progressHeight)
+        
+        /* 加载进度 */
+        let loadProgressWidth = loadValue * totalProgressView.frame.width
+        loadProgressView.layer.cornerRadius = progressHeight * 0.5
+        loadProgressView.frame = CGRect(origin: .zero, size: .init(width: loadProgressWidth, height: progressHeight))
+
+        /* 当前进度 */
+        let currentProgressWidth = currentValue * totalProgressView.frame.width
+        currentProgressView.layer.cornerRadius = progressHeight * 0.5
+        currentProgressView.frame = CGRect(origin: .zero, size: .init(width: currentProgressWidth, height: progressHeight))
+
+        /* 拖动控制块 */
+        let dragControlX = currentValue * totalProgressView.frame.width
+        dragControlView.frame = CGRect(origin: .init(x: dragControlX, y: 0),
+                                       size: .init(width: bounds.height, height: bounds.height))
+        
+        /* 拖动显示视图 */
+        controlView.bounds.size = controlSize
+        controlView.center = CGPoint(x: dragControlView.frame.width * 0.5, y: dragControlView.frame.height * 0.5)
+        
+    }
+}
+
+
+//MARK: - UI ------------------------------------------------------------------
+extension FDMProgressView {
+    private func createUI() {
+        self.addSubview(totalProgressView)
+        self.totalProgressView.addSubview(loadProgressView)
+        self.totalProgressView.addSubview(currentProgressView)
+        self.addSubview(dragControlView)
+        self.dragControlView.addSubview(controlView)
+        
+        /* 控制器默认颜色 */
+        totalProgressColor = .init(red: 207.0 / 225.0, green: 207.0 / 225.0, blue: 207.0 / 225.0, alpha: 1.0)
+        loadProgressColor = .init(red: 179.0 / 225.0, green: 179.0 / 225.0, blue: 179.0 / 225.0, alpha: 1.0)
+        currentProgressColor = .init(red: 102.0 / 225.0, green: 102.0 / 225.0, blue: 102.0 / 225.0, alpha: 1.0)
+        controlView.backgroundColor = .init(red: 244.0 / 225.0, green: 164.0 / 225.0, blue: 96.0 / 225.0, alpha: 1.0)
+        
+        controlSize = .init(width: progressHeight * 2, height: progressHeight * 2)
+        controlView.layer.cornerRadius = progressHeight
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        resetProgressView()
+    }
+}
+
+
+//MARK: - PrivateAction ------------------------------------------------------------------
+extension FDMProgressView {
+    private func createAction() {
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(dragWithLongGestureGesture(_:)))
+        longGesture.minimumPressDuration = 0
+        dragControlView.addGestureRecognizer(longGesture)
+    }
+    
+    /**
+     private - 长按控制块
+     */
+    @objc private func dragWithLongGestureGesture(_ gesture: UILongPressGestureRecognizer) {
+        
+        switch gesture.state {
+        case .began:
+            shouldChangeProgress = false
+            startValue = gesture.location(in: self).x
+            dragControlX = dragControlView.frame.origin.x
+            
+            self.delegate?.progressView?(self, didTouchBeginDragView: currentValue)
+            break
+            
+        case .changed:
+            guard startValue != nil && dragControlX != nil else { return }
+            let changeX = gesture.location(in: self).x
+            var originX = dragControlX! + (changeX - startValue!) * dragSpeed
+            
+            if originX <= 0 { originX = 0 }
+            if originX >= self.bounds.width - self.bounds.height { originX = self.bounds.width - self.bounds.height }
+            dragControlView.frame.origin.x = originX
+            setCurrentProgress()
+            
+            self.delegate?.progressView(self, didDraggingProgressView: currentValue)
+            break
+            
+        case .ended:
+            startValue = nil
+            dragControlX = nil
+            
+            self.delegate?.progressView?(self, didTouchEndDragView: currentValue)
+            self.shouldChangeProgress = true
+            break
+            
+        case .cancelled:
+            startValue = nil
+            dragControlX = nil
+            
+            self.delegate?.progressView?(self, didTouchEndDragView: currentValue)
+            self.shouldChangeProgress = true
+            break
+            
+        case .failed:
+            startValue = nil
+            dragControlX = nil
+            
+            self.delegate?.progressView?(self, didTouchEndDragView: currentValue)
+            self.shouldChangeProgress = true
+            break
+            
+        case .possible:
+            break
+            
+        @unknown default:
+            break
+        }
+    }
+    
+    /**
+     private - 设置当前进度条
+     */
+    private func setCurrentProgress() {
+        let progressWidth = dragControlView.center.x - totalProgressView.frame.origin.x
+        currentProgressView.frame = CGRect(origin: .zero, size: .init(width: progressWidth, height: progressHeight))
+        
+        /* 计算进度值 */
+        currentValue = progressWidth / totalProgressView.bounds.width
+    }
+    
+    /**
+     private - 设置拖动子控件的center
+     */
+    private func setSubControlViewCenter() {
+        controlView.bounds.size = controlSize
+    }
+}
--- FDMProgressViewDemo/ViewController.swift
+++ FDMProgressViewDemo/ViewController.swift
@@ -7,13 +7,207 @@
 
 import UIKit
 
+//MARK: - 进度条
 class ViewController: UIViewController {
+    
+    /* -------------------- */
+    let progressView = FDMProgressView()
+    var subControlView = UIImageView()
+    var value: CGFloat = 0  // 动态进度
+    /* -------------------- */
+    
+    /* 其他 */
+    let currentValueLabel = UILabel()
+    let currentValueField = UITextField()
+    
+    let loadValueLabel = UILabel()
+    let loadValueField = UITextField()
+    
+    var switchTitle_1 = UILabel()
+    var switch_1 = UISwitch()   // 模拟动态进度
+    
+    var switchTitle_2 = UILabel()
+    var switch_2 = UISwitch()   // 进度条放大缩小
+    
+    var switchTitle_3 = UILabel()
+    var switch_3 = UISwitch()   // 拖动按钮放大缩小
+    
+    var switchTitle_4 = UILabel()
+    var switch_4 = UISwitch()   // 拖动按钮放大缩小
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "进度条"
+        self.view.backgroundColor = .white
+        
+        createUI()
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
+//MARK: - UI
+extension ViewController {
+    func createUI() {
+        self.view.addSubview(progressView)
+        
+        /* 进度条（本区域为进度条使用方法） ------------------------------ */
+        progressView.delegate = self
+        progressView.progressHeight = 5
+        progressView.currentProgressColor = .orange
+        progressView.frame = CGRect(x: 15, y: 150, width: view.bounds.width - 30, height: 25)
+        
+        /* 创建进度条拖动View */
+        setControlSubView()
+        
+        /*  ----------------------------------- */
+            
+        /* 创建其他UI */
+        createOther()
+    }
+    
+    /**
+     设置子控制视图
+     */
+    func setControlSubView() {
+        subControlView.image = UIImage(named: "player_full_settings")
+        progressView.setControlSubView(subControlView, size: .init(width: 18, height: 18))
+    }
+}
+
+//MARK: - 进度条代理
+extension ViewController: FDMProgressViewDelegate {
+    /// 进度条正在拖动时回调
+    func progressView(_ progressView: FDMProgressView, didDraggingProgressView currentValue: CGFloat) {
+        currentValueField.text = "\(Int(currentValue * 100))"
+        value = currentValue
+    }
+    
+    /// 开始点击拖动视图时回调
+    func progressView(_ progressView: FDMProgressView, didTouchBeginDragView currentValue: CGFloat) {
+        UIView.animate(withDuration: 0.15) { [weak self] in
+            if self?.switch_2.isOn ?? false { progressView.progressHeight = 10 }
+            if self?.switch_3.isOn ?? false { progressView.controlSize = .init(width: 35, height: 35) }
+            if self?.switch_4.isOn ?? false { progressView.currentProgressColor = .purple }
+        }
+    }
+    
+    /// 停止点击拖动视图时回调
+    func progressView(_ progressView: FDMProgressView, didTouchEndDragView currentValue: CGFloat) {
+        UIView.animate(withDuration: 0.15) { [weak self] in
+            if self?.switch_2.isOn ?? false { progressView.progressHeight = 5 }
+            if self?.switch_3.isOn ?? false { progressView.controlSize = .init(width: 18, height: 18) }
+            if self?.switch_4.isOn ?? false { progressView.currentProgressColor = .orange }
+        }
+    }
+}
+
+//MARK: - Other
+extension ViewController: UITextFieldDelegate {
+    func createOther() {
+        self.view.addSubview(currentValueLabel)
+        self.view.addSubview(currentValueField)
+        self.view.addSubview(loadValueLabel)
+        self.view.addSubview(loadValueField)
+        self.view.addSubview(switchTitle_1)
+        self.view.addSubview(switch_1)
+        self.view.addSubview(switchTitle_2)
+        self.view.addSubview(switch_2)
+        self.view.addSubview(switchTitle_3)
+        self.view.addSubview(switch_3)
+        self.view.addSubview(switchTitle_4)
+        self.view.addSubview(switch_4)
+        
+        /* 当前进度值标题 */
+        currentValueLabel.text = "当前进度值"
+        currentValueLabel.frame = CGRect(x: 15, y: 200, width: 100, height: 30)
+        
+        /* 当前进度值输入框 */
+        currentValueField.delegate = self
+        currentValueField.placeholder = "请输入 1 - 100之间的值"
+        currentValueField.keyboardType = .numberPad
+        currentValueField.frame = CGRect(x: 150, y: 200, width: 210, height: 30)
+        currentValueField.layer.borderWidth = 0.5
+        currentValueField.layer.borderColor = UIColor.darkGray.cgColor
+        currentValueField.layer.cornerRadius = 8
+        currentValueField.layer.masksToBounds = true
+        
+        /* 加载进度值标题 */
+        loadValueLabel.text = "加载进度值"
+        loadValueLabel.frame = CGRect(x: 15, y: 250, width: 100, height: 30)
+        
+        /* 当前加载进度输入框 */
+        loadValueField.delegate = self
+        loadValueField.placeholder = "请输入 1 - 100之间的值"
+        loadValueField.keyboardType = .numberPad
+        loadValueField.frame = CGRect(x: 150, y: 250, width: 210, height: 30)
+        loadValueField.layer.borderWidth = 0.5
+        loadValueField.layer.borderColor = UIColor.darkGray.cgColor
+        loadValueField.layer.cornerRadius = 8
+        loadValueField.layer.masksToBounds = true
+        
+        /* 开启动态进度 */
+        switchTitle_1.text = "递归模拟动态进度"
+        switchTitle_1.frame = CGRect(x: 15, y: 300, width: 150, height: 30)
+        
+        /* 动态进度开关 */
+        switch_1.frame = CGRect(x: 200, y: 300, width: 100, height: 30)
+        switch_1.addTarget(self, action: #selector(clickSwitch_1(sender:)), for: .touchUpInside)
+        
+        /* 进度条放大缩小 */
+        switchTitle_2.text = "进度条放大与缩小"
+        switchTitle_2.frame = CGRect(x: 15, y: 350, width: 150, height: 30)
+        
+        /* 进度条放大缩小 */
+        switch_2.frame = CGRect(x: 200, y: 350, width: 100, height: 30)
+        
+        /* 拖动按钮放大缩小 */
+        switchTitle_3.text = "拖动按钮放大缩小"
+        switchTitle_3.frame = CGRect(x: 15, y: 400, width: 150, height: 30)
+        
+        /* 拖动按钮放大缩小 */
+        switch_3.frame = CGRect(x: 200, y: 400, width: 100, height: 30)
+        
+        /* 拖动修改进度颜色 */
+        switchTitle_4.text = "拖动修改进度颜色"
+        switchTitle_4.frame = CGRect(x: 15, y: 450, width: 150, height: 30)
+        
+        /* 拖动修改进度颜色 */
+        switch_4.frame = CGRect(x: 200, y: 450, width: 100, height: 30)
+    }
+    
+    /**
+     递归模拟动态进度
+     */
+    func changeProgress() {
+        if !switch_1.isOn {return}
+        if self.value > 1 { self.value = 0 }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+            self.value += 0.01
+            self.progressView.setCurrentProgressValue(self.value)
+            self.changeProgress()
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        var value: CGFloat = CGFloat(((textField.text ?? "") as NSString).intValue)
+        if value <= 0 { value = 0 }
+        if value >= 100 { value = 100 }
+        
+        if textField == currentValueField {
+            progressView.setCurrentProgressValue(value / 100)
+        }else if textField == loadValueField {
+            progressView.setLoadProgressValue(value / 100)
+        }
+    }
+    
+    /**
+     点击动态进度开关
+     */
+    @objc func clickSwitch_1(sender: UISwitch) {
+        if sender.isOn { changeProgress() }
+    }
+}
